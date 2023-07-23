4f4e183cdee4de5d2b123ca72e71185a5eabb7c2
arilhr <aurielharlih9213@gmail.com>
2023-07-24 00:21:33 +0700

    add aos in about us

--- src/components/homepage/about-us/index.tsx
+++ src/components/homepage/about-us/index.tsx
@@ -1,21 +1,43 @@
-import React from "react";
+"use client";
+
+import React, { useEffect } from "react";
+import Aos from "aos";
 
 const AboutUs = () => {
+  useEffect(() => {
+    Aos.init({ duration: 1000, once: true });
+  }, []);
+
   return (
     <div>
-      <h3 className="mb-12 text-center text-3xl font-bold text-white lg:text-start lg:text-7xl">
+      <h3
+        className="mb-12 text-center text-3xl font-bold text-white lg:text-start lg:text-7xl"
+        data-aos="fade-up"
+      >
         About Us
       </h3>
-      <div className="flex flex-col gap-6 text-center text-lg text-white lg:text-start lg:text-2xl">
-        <p>
+      <div className="about-us-aos flex flex-col gap-6 text-center text-lg text-white lg:text-start lg:text-2xl">
+        <p
+          data-aos="fade-up"
+          data-aos-delay="200"
+          data-aos-anchor=".about-us-aos"
+        >
           We are a networking company operating in the industry for the past 15
           years,partnering & working alongside major domestic carriers.
         </p>
-        <p>
+        <p
+          data-aos="fade-up"
+          data-aos-delay="400"
+          data-aos-anchor=".about-us-aos"
+        >
           We procure, purchase, lease & acquire(by other means) IP resources
           that clients call “residential”.
         </p>
-        <p>
+        <p
+          data-aos="fade-up"
+          data-aos-delay="600"
+          data-aos-anchor=".about-us-aos"
+        >
           These IPs are regarded as some ofthe best in the industry, allowing
           clients to complete a myriad of automated & scripted tasksonline.
         </p>
--- src/components/homepage/partners/index.tsx
+++ src/components/homepage/partners/index.tsx
@@ -35,24 +35,29 @@ const PartnersData = [
 
 const Partners = () => {
   return (
-    <div className="grid w-full grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-5">
+    <div className="partners-aos grid w-full grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-5">
       {PartnersData.map((item, index) => {
         const isOdd = Math.ceil((index + 1) / 2) % 2 === 1; // if 1 col start 2, else col start 1
         const modulus = Math.ceil((index + 1) % 2) === 1; // if 1, with col start
         return (
           <div
-            key={index}
             className={`${
               modulus && isOdd ? "lg:col-start-2" : ""
-            } group flex items-center justify-center rounded-lg bg-white p-4 opacity-70 transition-all duration-300 hover:opacity-100 lg:col-span-2 lg:px-10 lg:py-6`}
+            } group lg:col-span-2`}
+            key={index}
+            data-aos="fade-left"
+            data-aos-delay={index * 200}
+            data-aos-anchor=".partners-aos"
           >
-            <Image
-              src={item.logo}
-              alt={item.name}
-              width={200}
-              height={200}
-              className="min-h-full min-w-full object-contain object-center"
-            />
+            <div className="flex h-full w-full items-center justify-center rounded-lg bg-white p-4 opacity-70 transition-all duration-300 hover:opacity-100 lg:px-10 lg:py-6 ">
+              <Image
+                src={item.logo}
+                alt={item.name}
+                width={200}
+                height={200}
+                className="min-h-full min-w-full object-contain object-center"
+              />
+            </div>
           </div>
         );
       })}
